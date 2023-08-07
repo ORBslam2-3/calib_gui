5c0c690cc166bf8518654b266fe2997602dfd7cc
wxd <1006103577@qq.com>
2023-08-07 17:56:59 +0800

    8-7

--- calib_ws/build/ros_qt_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wxd/calib_gui/calib_ws/src/ros_qt_demo
-# Build directory: /home/wxd/calib_gui/calib_ws/build/ros_qt_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
